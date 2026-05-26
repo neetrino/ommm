f58bb9aaaacf89475e6869aab1506db9402431cb
ginosyan00 <gurgenginosyan@neetrino.com>
2026-05-26 17:25:23 +0400

    kayqum default english drecinq

--- apps/web/src/i18n/routing.ts
+++ apps/web/src/i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["hy", "ru", "en"],
-  defaultLocale: "hy",
+  defaultLocale: "en",
   localePrefix: "always",
 });
